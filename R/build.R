76c6049d59aaa7faa18f5ba6e87418dee828958b
Alessandro Samuel-Rosa <alessandrosamuelrosa@gmail.com>
2020-06-30 18:40:45 -0300

    adds files for creating spatial visualization

--- R/build.R
+++ R/build.R
@@ -1,2 +1,10 @@
 blogdown::build_dir("static")
 
+# Editar a linha 92 do arquivo HTML, substituindo
+file <- 'static/projeto/febr/espacial/index.html'
+html <- readLines(con = file)
+for (i in seq(length(html))) {
+  html[i] <- gsub(pattern = '940px', replacement = '100%', x = html[i], fixed = TRUE)
+}
+html <- sub("<title>index.utf8.md</title>", "<title>FEBR | Visualização Espacial</title>", html)
+writeLines(text = html, con = file)
