f4e995792dea66e9995d612a1a71afdb7a61f78f
samuel-rosa <alessandrosamuelrosa@gmail.com>
2023-01-17 00:18:30 -0300

    update febr

---
+++ R/build-febr-busca.R
@@ -0,0 +1,93 @@
+# Repositório de Dados do Solo Brasileiro ##########################################################
+
+# Página de busca febr/busca/
+# Preparar arquivo TXT com dados no formato JSON
+# A página de busca utiliza DataTables
+catalogo <- openxlsx::read.xlsx("/home/alessandrorosa/ownCloud/febr-repo/publico/febr-indice.xlsx")
+catalogo$dados_id <- paste0('<a href="../', catalogo$dados_id, '/">', catalogo$dados_id, '</a>')
+catalogo <- jsonlite::toJSON(
+  list(data = catalogo[c("dados_id", "dados_titulo", "dados_autor")]), pretty = TRUE)
+writeLines(catalogo, con = "content/pt/febr/buscar/catalogo.txt")
+
+# Páginas individuais dos conjuntos de dados do FEBR
+# 1. Criar um diretório para cada conjunto de dados
+ctb <- list.dirs("~/ownCloud/febr-repo/publico", full.names = FALSE)[-c(1:2)]
+dirs <- dir.exists(paste0("content/pt/febr/dados/", ctb))
+cmd <- paste0("mkdir content/pt/febr/dados/", ctb[!dirs])
+lapply(cmd, system)
+
+# 2. Copiar os metadados de citação
+cmd <- paste0(
+  "cp ~/ownCloud/febr-repo/publico/", ctb, "/", ctb, "-identificacao.txt content/pt/febr/dados/", ctb)
+lapply(cmd, system)
+
+# 3. Criar index.md para cada conjunto de dados
+for (i in 1:length(ctb)) {
+  identificacao <- read.table(
+    paste0("content/pt/febr/dados/", ctb[i], "/", ctb[i], "-identificacao.txt"),
+    stringsAsFactors = FALSE, header = TRUE)
+  rownames(identificacao) <- identificacao$campo
+  # Disfarçar endereços de e-mail
+  identificacao["dados_autor", "valor"] <- gsub("@", " em ", identificacao["dados_autor", "valor"])
+  identificacao["dados_autor", "valor"] <-
+    gsub(".com", " com", identificacao["dados_autor", "valor"], fixed = TRUE)
+  identificacao["dados_autor", "valor"] <-
+    gsub(".br", " br", identificacao["dados_autor", "valor"], fixed = TRUE)
+  identificacao["dados_autor", "valor"] <- 
+    gsub(" (NA)", "", identificacao["dados_autor", "valor"], fixed = TRUE)
+  identificacao["dados_autor", "valor"] <-
+    gsub(" (xx)", "", identificacao["dados_autor", "valor"], fixed = TRUE)
+  write.table(
+    identificacao, paste0("content/pt/febr/dados/", ctb[i], "/", ctb[i], "-identificacao.txt"), 
+    row.names = FALSE)
+
+  index_template <- readLines("content/pt/febr/buscar/index_template.txt")
+  index_template <- sub("dados_id", ctb[i], index_template)
+  index_template <- sub("dados_id", ctb[i], index_template)
+  # Título
+  index_template <- sub("dados_titulo", identificacao["dados_titulo", "valor"], index_template)
+  # Autor(es)
+  dados_autor <- identificacao["dados_autor", "valor"]
+  if (is.na(dados_autor)) {
+    dados_autor <- "[]"
+  }
+  dados_autor <- gsub("; ", '", "', dados_autor)
+  dados_autor <- gsub(" \\(.*?\\)", "", dados_autor)
+  dados_autor <- paste0('"', dados_autor, '"')
+  index_template <- gsub("dados_autor", dados_autor, index_template)
+  # Tamanho do arquivo
+  exc <- paste0("~/ownCloud/febr-repo/publico/", ctb[i], "/", ctb[i], ".xlsx")
+  size_xlsx <- ifelse(file.exists(exc), paste(file.size(exc), "bytes"), "ARQUIVO INDISPONÍVEL")
+  index_template <- sub("size_xlsx", size_xlsx, index_template)
+
+  ide <- paste0("~/ownCloud/febr-repo/publico/", ctb[i], "/", ctb[i], "-identificacao.txt")
+  size_identificacao <- ifelse(
+    file.exists(ide), paste(file.size(ide), "bytes"), "ARQUIVO INDISPONÍVEL")
+  index_template <- sub("size_identificacao", size_identificacao, index_template)
+  
+  ver <- paste0("~/ownCloud/febr-repo/publico/", ctb[i], "/", ctb[i], "-versionamento.txt")
+  size_versionamento <- ifelse(
+    file.exists(ver), paste(file.size(ver), "bytes"), "ARQUIVO INDISPONÍVEL")
+  index_template <- sub("size_versionamento", size_versionamento, index_template)
+  
+  met <- paste0("~/ownCloud/febr-repo/publico/", ctb[i], "/", ctb[i], "-metadado.txt")
+  size_metadado <- ifelse(file.exists(met), paste(file.size(met), "bytes"), "ARQUIVO INDISPONÍVEL")
+  index_template <- sub("size_metadado", size_metadado, index_template)
+  
+  obs <- paste0("~/ownCloud/febr-repo/publico/", ctb[i], "/", ctb[i], "-observacao.txt")
+  size_observacao <- ifelse(
+    file.exists(obs), paste(file.size(obs), "bytes"), "ARQUIVO INDISPONÍVEL")
+  index_template <- sub("size_observacao", size_observacao, index_template)
+  
+  cam <- paste0("~/ownCloud/febr-repo/publico/", ctb[i], "/", ctb[i], "-camada.txt")
+  size_camada <- ifelse(file.exists(cam), paste(file.size(cam), "bytes"), "ARQUIVO INDISPONÍVEL")
+  index_template <- sub("size_camada", size_camada, index_template)
+  
+  # Palavras chave
+  palavras_chave <- identificacao["palavras_chave", "valor"]
+  if (is.na(palavras_chave)) {
+    palavras_chave <- "Dados, Dados Legados, Repositório de Dados, Base de Dados, Dados Abertos"
+  }
+  index_template <- gsub("palavras_chave", gsub(";", ",", palavras_chave), index_template)
+  writeLines(text = index_template, con = paste0("content/pt/febr/dados/", ctb[i], "/index.md"))
+}
--- content/pt/febr/dados/ctb0683/index.md
+++ content/pt/febr/dados/ctb0683/index.md
@@ -4,7 +4,7 @@ date: 2020-10-28
 slug: ../ctb0683
 summary: Repositório Brasileiro Livre para Dados Abertos do Solo (FEBR) | A febre dos dados de solo no Brasil
 #authors: ["Nilson Rendeiro Pereira", "César da Silva Chagas", "Fernando Cézar S. do Amaral", "Alexandre Ortega", "Maria José Zaroni", "Mário Luiz Diamante Áglio", "Waldir de Carvalho Júnior", "Ailton Martins Amorim", "Silvio Barge Bhering", "Antonio Carlos Borges Daniel Filho", "Enio Fraga da Silva", "Carlos Henrique Lemos Lopes", "Tony Jarbas Ferreira Cunha", "João Sotoya Takagi"]
-tags: [Dados, Dados Legados, Repositório de Dados, Base de Dados, Dados Abertos]
+tags: [Levantamento de Solos, Zoneamento Agroecológico, Mato Grosso do Sul]
 ---
 
 <style>
@@ -52,42 +52,42 @@ Use este identificador para citar ou linkar para este item: `https://www.pedomet
     <tr>
       <td>ctb0683.xlsx</td>
       <td>Coleção de todas as tabelas que compõe o conjunto de dados</td>
-      <td>208320 bytes</td>
+      <td>209726 bytes</td>
       <td>XLSX — Planilha do Office Open XML</td>
       <td><a href="https://cloud.utfpr.edu.br/index.php/s/Df6dhfzYJ1DDeso/download?path=%2Fctb0683&files=ctb0683.xlsx" class="btn btn-primary btn-block" role="button">Visualizar/Abrir</a></td>
     </tr>
     <tr>
       <td>ctb0683-identificacao.txt</td>
       <td>Metadados de citação</td>
-      <td>3635 bytes</td>
+      <td>1735 bytes</td>
       <td>TXT — Texto plano com valores separados por tabulação</td>
       <td><a href="https://cloud.utfpr.edu.br/index.php/s/Df6dhfzYJ1DDeso/download?path=%2Fctb0683&files=ctb0683-identificacao.txt" class="btn btn-primary btn-block" role="button">Visualizar/Abrir</a></td>
     </tr>
     <tr>
       <td>ctb0683-versionamento.txt</td>
       <td>Histórico de modificações</td>
-      <td>746 bytes</td>
+      <td>2443 bytes</td>
       <td>TXT — Texto plano com valores separados por tabulação</td>
       <td><a href="https://cloud.utfpr.edu.br/index.php/s/Df6dhfzYJ1DDeso/download?path=%2Fctb0683&files=ctb0683-versionamento.txt" class="btn btn-primary btn-block" role="button">Visualizar/Abrir</a></td>
     </tr>
     <tr>
       <td>ctb0683-metadado.txt</td>
       <td>Metadados de métodos analíticos</td>
-      <td>12461 bytes</td>
+      <td>12616 bytes</td>
       <td>TXT — Texto plano com valores separados por tabulação</td>
       <td><a href="https://cloud.utfpr.edu.br/index.php/s/Df6dhfzYJ1DDeso/download?path=%2Fctb0683&files=ctb0683-metadado.txt" class="btn btn-primary btn-block" role="button">Visualizar/Abrir</a></td>
     </tr>
     <tr>
       <td>ctb0683-observacao.txt</td>
       <td>Dados dos locais de observação do solo (pontos, perfis)</td>
-      <td>35663 bytes</td>
+      <td>35783 bytes</td>
       <td>TXT — Texto plano com valores separados por tabulação</td>
       <td><a href="https://cloud.utfpr.edu.br/index.php/s/Df6dhfzYJ1DDeso/download?path=%2Fctb0683&files=ctb0683-observacao.txt" class="btn btn-primary btn-block" role="button">Visualizar/Abrir</a></td>
     </tr>
     <tr>
       <td>ctb0683-camada.txt</td>
       <td>Dados das amostras do solo (camadas, horizontes)</td>
-      <td>118549 bytes</td>
+      <td>119136 bytes</td>
       <td>TXT — Texto plano com valores separados por tabulação</td>
       <td><a href="https://cloud.utfpr.edu.br/index.php/s/Df6dhfzYJ1DDeso/download?path=%2Fctb0683&files=ctb0683-camada.txt" class="btn btn-primary btn-block" role="button">Visualizar/Abrir</a></td>
     </tr>
